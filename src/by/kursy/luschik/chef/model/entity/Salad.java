5232229f0bbc24a11db7638bf5f9084c3d3e1d87
nice-gift <dmitry.luschick@yandex.by>
2022-07-08 12:15:10 +0300

    added ArrayList<> to Salad

--- src/by/kursy/luschik/chef/model/entity/Salad.java
+++ src/by/kursy/luschik/chef/model/entity/Salad.java
@@ -1,50 +1,48 @@
 package by.kursy.luschik.chef.model.entity;
 
+import java.util.ArrayList;
+
 public class Salad {
-    private Vegetable[] vegetables;
+    private ArrayList<Vegetable> vegetables;
 
     public Salad() {
-        vegetables = new Vegetable[0];
+        vegetables = new ArrayList<>();
     }
 
     public Salad(Vegetable[] vegetables) {
         if (vegetables != null) {
-            this.vegetables = vegetables;
-        }
-        else vegetables = new Vegetable[0];
+            this.vegetables = new ArrayList<>();
+            for (Vegetable vegetable : vegetables
+            ) {
+                this.vegetables.add(vegetable);
+            }
+        } else vegetables = new Vegetable[0];
     }
 
     public int size() {
-        return vegetables.length;
+        return vegetables.size();
     }
 
     public Vegetable get(int index) {
-        if (index >= 0 && index < vegetables.length) {
-            return vegetables[index];
+        if (index >= 0 && index < vegetables.size()) {
+            return vegetables.get(index);
         } else return null;
     }
 
     public void add(Vegetable vegetable) {
-        Vegetable[] temp = new Vegetable[vegetables.length + 1];
-
-        for (int i = 0; i < vegetables.length; i++) {
-            temp[i] = vegetables[i];
-        }
-        temp[vegetables.length] = vegetable;
-        vegetables = temp;
+        vegetables.add(vegetable);
     }
 
     public void remove(Vegetable vegetable) {
         boolean flag = false;
-        Vegetable[] temp = new Vegetable[vegetables.length - 1];
+        ArrayList<Vegetable> temp = new ArrayList<>();
 
-        for (int i = 0, j = 0; i < vegetables.length; i++) {
-            if (flag || !vegetables[i].getName().equals(vegetable.getName()) ||
-                    vegetables[i].getWeight() != vegetable.getWeight() ||
-                    vegetables[i].getPrice() != vegetable.getPrice() ||
-                    vegetables[i].getCalories() != vegetable.getCalories()) {
-                temp[j] = vegetables[i];
-                j++;
+        for (int i = 0; i < vegetables.size(); i++) {
+            if (flag || !vegetables.get(i).getName().equals(vegetable.getName()) ||
+                    vegetables.get(i).getWeight() != vegetable.getWeight() ||
+                    vegetables.get(i).getPrice() != vegetable.getPrice() ||
+                    vegetables.get(i).getCalories() != vegetable.getCalories()) {
+                temp.add(vegetables.get(i));
             } else {
                 flag = true;
             }
@@ -53,12 +51,11 @@ public class Salad {
     }
 
     public void remove(int index) {
-        Vegetable[] temp = new Vegetable[vegetables.length - 1];
+        ArrayList<Vegetable> temp = new ArrayList<>();
 
-        for (int i = 0, j = 0; i < vegetables.length; i++) {
+        for (int i = 0; i < vegetables.size(); i++) {
             if (i != index) {
-                temp[j] = vegetables[i];
-                j++;
+                temp.add(vegetables.get(i));
             }
         }
         vegetables = temp;
