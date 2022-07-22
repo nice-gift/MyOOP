8daa69c9a456570f39a88b4b0c22fb7d676703af
nice-gift <dmitry.luschick@yandex.by>
2022-07-22 08:36:33 +0300

    updated

--- src/by/kursy/luschik/chef/controller/Main.java
+++ src/by/kursy/luschik/chef/controller/Main.java
@@ -3,8 +3,17 @@ package by.kursy.luschik.chef.controller;
 //  Шеф-повар (Chef). Необходимо приготовить овощной салат и определить вес
 //  салата и его калорийность, а также найти самый калорийных овощ в салате.
 
+import by.kursy.luschik.chef.model.entity.Salad;
+import by.kursy.luschik.chef.model.entity.Vegetable;
+
 public class Main {
     public static void main(String[] args) {
+        Vegetable[] vegetables = {
+                new Vegetable("Tomato", 140, 0.6, 30),
+                new Vegetable("Tomato", 140, 0.6, 30),
+                new Vegetable("Tomato", 140, 0.6, 30)};
 
+        Salad salad = new Salad(vegetables);
+        System.out.println(salad);
     }
 }
