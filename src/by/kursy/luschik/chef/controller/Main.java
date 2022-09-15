a9bdec66054051fb827010613a464dd6a4f25989
nice-gift <100232175+nice-gift@users.noreply.github.com>
2022-09-15 23:07:18 +0300

    up

--- src/by/kursy/luschik/chef/controller/Main.java
+++ src/by/kursy/luschik/chef/controller/Main.java
@@ -4,33 +4,33 @@ package by.kursy.luschik.chef.controller;
 //  салата и его калорийность, а также найти самый калорийных овощ в салате.
 
 import by.kursy.luschik.chef.model.entity.*;
+import by.kursy.luschik.chef.model.logic.Cook;
 import by.kursy.luschik.chef.model.util.Serializer;
 
 public class Main {
-    //    public static void main(String[] args) {
-//        Tomato tomato= new Tomato("Tomato", 140, 0.6, 30, Color.IMPERIAL, 3);
-//        tomato.setColor(Color.RED);
-//        System.out.println(tomato);
-//
-//        Vegetable[] vegetables = {
-////                new Tomato("Tomato", 140, 0.6, 30, Color.IMPERIAL, 3),
-//                new Vegetable("Tomato", 140, 0.6, 30),
-//                new Vegetable("Tomato", 140, 0.6, 30)};
-//
-//        Salad salad = new Salad(vegetables);
-//        System.out.println(salad);
-//    }
     public static void main(String[] args) {
         Salad salad = new Salad();
         salad.add(new Cucumber("Tanja", 108, 0.5, 26, PeelPattern.RUSSIAN));
-        salad.add(new Tomato("Early Girl", 136, 0.6, 29, Color.IMPERIAL, 5));
-        salad.add(new Tomato("Early Girl", 140, 0.6, 30, Color.IMPERIAL, 6));
+        salad.add(new Tomato("Early Girl", 136, 0.6, 29, ColorTomato.IMPERIAL, 5));
+        salad.add(new Tomato("Early Girl", 140, 0.6, 30, ColorTomato.IMPERIAL, 6));
         salad.add(new Cucumber("Tanja", 100, 0.5, 25, PeelPattern.RUSSIAN));
         salad.add(new Cucumber("Spacemaster", 85, 0.55, 28, PeelPattern.GERMAN));
 
-        Serializer serializer = new Serializer();
+        Vegetable[] vegetables = new Vegetable[10];
 
-        serializer.serialize(salad);
-        System.out.println(serializer.deserialize());
+        for (int i = 0; i < 5; i++) {
+            vegetables[i] = new Cucumber("Tanja", 108, 0.5, 26, PeelPattern.RUSSIAN);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            vegetables[i] = new Tomato("Early Girl", 140, 0.6, 30, ColorTomato.IMPERIAL, 6);
+        }
+
+        System.out.println(salad);
+        System.out.println(Cook.calcTotalWeight(salad));
+//        Serializer serializer = new Serializer();
+//
+//        serializer.serialize(salad);
+//        System.out.println(serializer.deserialize());
     }
 }
